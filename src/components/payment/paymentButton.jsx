fa02bcb73f22b74d27105b9cfeb8d91f3da0e623
Ansif <kiichu7034@gmail.com>
2025-11-20 23:47:12 +0530

    Updated

--- src/components/payment/paymentButton.jsx
+++ src/components/payment/paymentButton.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 export const handlePayment =async (books)=>{
         try{
             const bookArray =Array.isArray(books)? books :[books];
-            const {data} =await axios.post("http://localhost:5000/payment/create-checkout-session",
+            const {data} =await axios.post(`${process.env.NEXT_PUBLIC_FETCH_DATA_URL}/payment/create-checkout-session`,
                {books:bookArray,}
             )
             window.location.href =data.url
